b4652ab380cdc2d3627d95fec3f8aef8dfb7e15b
DaveP80 <107325159+DaveP80@users.noreply.github.com>
2024-12-31 14:28:53 -0500

    ssl

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 
 const cn = {
   host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
   user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD
+  password: process.env.PG_PASSWORD,
+  ssl: true
 };
 
 const db = pgp(cn);
